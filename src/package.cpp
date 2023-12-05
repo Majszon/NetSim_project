c9e317f572ca7e10e38f6e48f685bdfbd558fea1
MiloszAdamek <miloush00767@gmail.com>
2023-12-05 16:04:28 +0100

    class Package

--- src/package.cpp
+++ src/package.cpp
@@ -4,14 +4,20 @@
 #include "package.hpp"
 
 Package::Package(){
+
     ElementID new_id;
-    if (!freed_IDs.empty){
+    if (!freed_IDs.empty()){
         new_id = *freed_IDs.begin();
         freed_IDs.erase(new_id);
     }
     else{
-        new_id = assigned_IDs.edn() + 1;
+        new_id =  *assigned_IDs.rbegin() + 1;
     }
     assigned_IDs.insert(new_id);
     id_ = new_id;
+
+}
+
+ElementID Package::get_id() const {
+    return id_;
 }
