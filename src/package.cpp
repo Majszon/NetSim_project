846382ec7d0ced0d93684ad3e421ff429d4391c8
Majszon <szymonb@student.agh.edu.pl>
2023-12-06 19:33:56 +0100

    Pliki i cmake

--- src/package.cpp
+++ src/package.cpp
@@ -3,21 +3,32 @@
 //
 #include "package.hpp"
 
-Package::Package(){
+std::set<ElementID> Package::assigned_IDs = {};
+std::set<ElementID> Package::freed_IDs = {};
 
-    ElementID new_id;
-    if (!freed_IDs.empty()){
-        new_id = *freed_IDs.begin();
-        freed_IDs.erase(new_id);
+Package::Package() {
+    if (assigned_IDs.empty() && freed_IDs.empty()) {
+        ID_ = 1;
+    } else if (!freed_IDs.empty()) {
+        ID_ = *freed_IDs.begin();
+        freed_IDs.erase(*freed_IDs.begin());
+    } else if (!assigned_IDs.empty()) {
+        ID_ = *assigned_IDs.end() + 1;
     }
-    else{
-        new_id =  *assigned_IDs.rbegin() + 1;
-    }
-    assigned_IDs.insert(new_id);
-    id_ = new_id;
-
+    assigned_IDs.insert(ID_);
 }
 
-ElementID Package::get_id() const {
-    return id_;
+Package &Package::operator=(Package &&package)  noexcept {
+    if (this == &package)
+        return *this;
+    assigned_IDs.erase(this->ID_);
+    freed_IDs.insert(this->ID_);
+    this->ID_ = package.ID_;
+    assigned_IDs.insert(this->ID_);
+    return *this;
+}
+
+Package::~Package() {
+    freed_IDs.insert(ID_);
+    assigned_IDs.erase(ID_);
 }
