871dc751c8f19aac81aa978fa94aa6dbf6856e42
Majszon <szymonb@student.agh.edu.pl>
2023-12-12 14:36:12 +0100

    Podstawowe pliki

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -9,9 +9,16 @@
 #include "types.hpp"
 #include "storage_types.hpp"
 class IPackageReceiver{
+public:
     virtual void receive_package(Package && p) = 0;
     virtual ElementID get_id(void) const = 0;
 
+    virtual IPackageStockPile::const_iterator begin() const = 0;
+    virtual IPackageStockPile::const_iterator cbegin() const = 0;
+    virtual IPackageStockPile::const_iterator end() const = 0;
+    virtual IPackageStockPile::const_iterator cend() const = 0;
+
+    virtual ~IPackageReceiver() = default;
 };
 #endif //NETSIM_NODES_HPP
 // 1: Bugajski (414889), Adamek (414896), Basiura (414817)
