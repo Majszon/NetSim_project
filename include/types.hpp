16fa6a43bc6cb2c9e5589bf92307479182f591f8
Majszon <szymonb@student.agh.edu.pl>
2023-12-12 14:30:03 +0100

    Podstawowe pliki

--- include/types.hpp
+++ include/types.hpp
@@ -7,6 +7,8 @@
 #define NETSIM_TYPES_HPP
 
 using ElementID = int;
-
+using Time = unsigned int;
+using TimeOffset = unsigned int;
+using ProbabilityGenerator = std::function<double()>;
 #endif //NETSIM_TYPES_HPP
 // 1: Bugajski (414889), Adamek (414896), Basiura (414817)
